0488ca4b26f29a38d0b8bfcd7bd0e4c1c058bc94
ryzwann-ba <ryzwann.ba@gmail.com>
2017-11-05 16:19:34 +0100

    sqlite

---
+++ sqlite.py
@@ -0,0 +1,11 @@
+import sqlite3
+conn = sqlite3.Connection(ma_base.db)
+cursor = conn.cursor()
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+     id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+     login TEXT,
+     mdp text
+)
+""")
+conn.commit()
